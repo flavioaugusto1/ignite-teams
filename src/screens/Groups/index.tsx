559b782047dc209fde36ec6cd94df3e20b662076
Flávio <f.augustosdn.dev@gmail.com>
2023-10-19 23:04:00 -0300

    Implementado path mapping com babel plugin module resolver

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export function Groups() {
   return (
     <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx tabout:blank#blockedo start working on your app!</Text>
     </View>
   );
 }
