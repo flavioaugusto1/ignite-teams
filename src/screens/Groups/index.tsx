296a02ee514cf6ca3e0027a6515a8893280630a3
Flávio <f.augustosdn.dev@gmail.com>
2023-10-21 18:16:48 -0300

    Implementada flatlist para exibir cards das turmas ou mensagem caso não tenha turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
+
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
+
   return (
     <Container>
       <Header />
-      
-      <Highlight 
+
+      <Highlight
         title='Turmas'
         subTitle='Jogue com a sua turma'
       />
-      
-      <GroupCard title='Bonde da choffezada' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        )}
+      />
     </Container>
   );
 }
