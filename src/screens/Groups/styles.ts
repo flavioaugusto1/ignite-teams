d08a80b2dfd505562a9fde912fd213bba9c1b034
Flávio <f.augustosdn.dev@gmail.com>
2023-10-20 23:46:28 -0300

    Configurando para utilizar fontes personalizadas

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,15 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`
-
-export const Title = styled.Text`
-  color: #FFF;
-  padding: 10px;
-  font-size: 36px;
-  font-weight: bold;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `
