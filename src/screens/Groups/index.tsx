67bcde66138571e3875d1317eadbcab85e59caeb
Flávio <f.augustosdn.dev@gmail.com>
2023-10-21 18:52:09 -0300

    Feito componente de botão para criação e exclusão de turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 
-import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
-
 
   return (
     <Container>
@@ -32,6 +33,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message='Que tal cadastrar a primeira turma?' />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
