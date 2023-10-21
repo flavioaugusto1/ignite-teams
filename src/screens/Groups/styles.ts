4d891932505b7d2fdca0297e41343946b0500182
Flávio <f.augustosdn.dev@gmail.com>
2023-10-20 23:23:00 -0300

    Feito downgrade na versão do styled-components devido a bug de tipagem

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,13 +2,14 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 `
 
 export const Title = styled.Text`
   color: #FFF;
-  margin: auto;
-  text-align: center;
   padding: 10px;
   font-size: 36px;
   font-weight: bold;
