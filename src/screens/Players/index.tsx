0a286b8ea67f316621030df37948caa607a8c31d
Flávio <f.augustosdn.dev@gmail.com>
2023-10-22 21:22:17 -0300

    Criado listagem de players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,12 +6,15 @@ import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./style";
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(['Flávio', 'Isabel'])
 
   return (
     <Container>
@@ -50,6 +53,30 @@ export function Players() {
           {players.length}
         </NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Não jogadores nesse time' />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button
+        title='Remover turma'
+        type='SECONDARY'
+      />
     </Container>
   )
 }
