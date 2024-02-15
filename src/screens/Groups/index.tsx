06c9a88ff30872aa2024c783b8272f00100e9550
Flávio <f.augustosdn@gmail.com>
2024-02-15 16:43:00 -0300

    feat: agora ao criar uma nova turma, ela está sendo listada

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,50 +1,56 @@
-import { useState } from 'react';
-import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useState, useEffect, useCallback } from "react";
+import { FlatList } from "react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
-import { ListEmpty } from '@components/ListEmpty';
-import { Button } from '@components/Button';
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+import { groupGetAll } from "@storage/group/groupGetAll";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([])
-  const navigation = useNavigation()
+  const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate('new')
+    navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const group = await groupGetAll();
+      setGroups(group);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, []),
+  );
 
   return (
     <Container>
       <Header />
 
-      <Highlight
-        title='Turmas'
-        subTitle='Jogue com a sua turma'
-      />
+      <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
 
       <FlatList
         data={groups}
-        keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <GroupCard
-            title={item}
-          />
-        )}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
-          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
 
-      <Button
-        title='Criar nova turma'
-        onPress={handleNewGroup}
-      />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
